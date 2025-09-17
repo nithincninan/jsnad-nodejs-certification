dc62c5f78c2c8ffd73b6dbb33048d8fc9185ba1f
Nithin Ninan <nithincninan@gmail.com>
2025-09-17 15:18:48 +0530

    jsnad-nodejs-certification

--- Events/1.custom-message-queue-with-eventEmitter.js
+++ Events/1.custom-message-queue-with-eventEmitter.js
@@ -1,13 +1,14 @@
-//1. Custom Message Queue with EventEmitter
-// Write a Node.js script that creates a custom EventEmitter to simulate a message queue, emitting messageReceived when a message is added and messageProcessed when processed, 
-//  with multiple listeners logging message details and status.
+// 1. Custom Message Queue with EventEmitter
 
+// Write a Node.js script that creates a custom EventEmitter to simulate a message queue, 
+// emitting messageReceived when a message is added and messageProcessed when processed, 
+// with multiple listeners logging message details and status.
+
+'use strict'
 const EventEmitter = require('events');
 
 class MyEmitter extends EventEmitter {}
-
 const myEmitter = new MyEmitter();
-
 var queue = [];
 
 function receiveMessage(msg) {
@@ -17,7 +18,10 @@ function receiveMessage(msg) {
 
 function processMessage() {
     const intervalId = setInterval(() => {
+        //console.log('Message queue before:', queue);
         const msg = queue.shift();
+        //console.log('Message queue after:', queue);
+        //console.log('Message processed inside:', msg);
         myEmitter.emit('messageProcessed', msg);
         if (queue.length === 0) {
             clearInterval(intervalId);
@@ -38,3 +42,18 @@ myEmitter.on('messageProcessed', (msg) => {
 receiveMessage('First Message');
 receiveMessage('Second Message');
 processMessage();
+
+//Output:
+// Message received: First Message
+// Message received: Second Message
+// Message processed: First Message
+// Message processed: Second Message
+// All messages processed
+
+//IMP :
+//setInterval() is a built-in function used to run a piece of code repeatedly at fixed time intervals. With clearInterval() to stop it.
+//queue.shift() : Removes the first element from the array. Returns the removed element.
+//let queue = [1, 2, 3, 4];
+//let removed = queue.shift(); 
+//console.log(removed); // 1
+//console.log(queue);   // [2, 3, 4]
